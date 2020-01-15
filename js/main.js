6d8c1dfd16b6032e291a27c897ec71986ca32a90
Fatallez_VT <fatallez@rambler.ru>
2020-01-15 20:20:27 +0400

    .

--- js/main.js
+++ js/main.js
@@ -1,58 +1,98 @@
 'use strict';
 
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+// const getRequest = (url) => {
+//   return new Promise((resolve, reject ) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.onreadystatechange = () => {
+//       resolve(xhr.responseText);
+//       reject(console.error('err'));
+//     };
+//     xhr.send();
+//   });
+// };
+
 class ProductList {
   constructor(container = '.products') {
     this.container = container;
     this.goods = [];
+    this.cart = {};
     this.allProducts = [];
-    this._fetchProducts();
-    this.render();
+    this.addToCart();
+    this._getProducts()
+        .then(data => {
+          this.goods = [...data];
+          this.render();
+        });
   }
 
-  _fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook',  price: 20000, img:'img/notebook.jpg'},
-      {id: 2, title: 'Mouse',     price: 1500,  img: 'img/mouse.jpg'},
-      {id: 3, title: 'Keyboard',  price: 5000,  img: 'img/keyboard.jpg'},
-      {id: 4, title: 'Gamepad',   price: 4500,  img:'img/gamepad.jpg'},
-    ]
+  _getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        });
   }
 
   render() {
     const block = document.querySelector(this.container);
-
     for (let product of this.goods) {
       const productObject = new ProductItem(product);
       this.allProducts.push(productObject);
       block.insertAdjacentHTML('beforeend', productObject.render());
     }
   }
+
+  addToCart() {
+
+  }
 }
 
 class ProductItem {
-  constructor(product) {
-    this.id = product.id;
-    this.title = product.title;
+  constructor(product, img='https://placehold.it/200x200') {
+    this.id_product = product.id_product;
+    this.product_name = product.product_name;
     this.price = product.price;
-    this.img = product.img;
+    this.img = img;
   }
 
   render() {
-    return `<div class="product-item" data-id="${this.id}">
-              <h3 class="product__title">${this.title}</h3>
+    return `<div class="product-item" data-id="${this.id_product}">
               <img src='${this.img}' alt="alt" class="product__img">
+              <h3 class="product__title">${this.product_name}</h3>              
               <p class="product__price">${this.price} руб.</p>
-              <button class="by-btn">Добавить в корзину</button>
+              <button id="addToCart" data-id="${this.id_product}">Добавить в корзину</button>
             </div>`;
+  }
+
+
+}
+
+class CartList {
+  constructor(container = '.cart') {
+    this.container = container;
+    this.cartBlock = null;
+    this.clrCartButton = null;
+    this.catalogList = [];
+    this.goods = [];
+    this._getCartList();
+    this._removeCartItem();
+  }
+
+  _getCartList() {
+
+  }
+
+  _removeCartItem() {
+
   }
 }
 
-class CartList extends ProductList {
-    countCartPrice() {}
-}
-
-class CartItem extends ProductItem {
+class CartItem {
 
 }
 
 const list = new ProductList();
+const cartList = new CartList();
